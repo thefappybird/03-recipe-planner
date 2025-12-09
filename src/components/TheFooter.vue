6ef9c4afcf30686499215e1b35e4358be760a8e0
thefappybird <65710064+thefappybird@users.noreply.github.com>
2025-12-09 11:32:23 +0400

    Bind contact number and email to environment variables

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,11 +6,14 @@
         >Alexander A. Banaag III</a
       >
     </h2>
-    <h6>+971 50 240 7012</h6>
-    <h6>alex.banaag1@gmail.com</h6>
+    <h6>{{ contactNum }}</h6>
+    <h6>{{ email }}</h6>
   </footer>
 </template>
-
+<script>
+  const contactNum = import.meta.env.VITE_CONTACT_NUM;
+  const email = import.meta.env.VITE_EMAIL;
+</script>
 <style scoped lang="scss">
 footer {
   @include flex-type(column, flex-start, flex-start);
